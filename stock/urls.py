934e9acfe1dcb0e8f48241c3733ef658e9433597
imjeongjun <jjlim1004@naver.com>
2021-05-14 17:46:53 +0900

    5.14

--- stock/urls.py
+++ stock/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 app_name = 'stock'
+
 urlpatterns = [
-    path('', views.kospi, name='kospi'),
+    path('', views.stock, name='stock'),
+    path('kospi/', views.kospi, name='kospi'),
 ]
--- stock/views.py
+++ stock/views.py
@@ -1,4 +1,46 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import pandas_datareader as wb
+import pandas as pd
+import datetime
+import matplotlib.pyplot as plt
+
 
 # Create your views here.
+def stock(request):
+    return render(request, 'stock/kospi.html')
+
+
 def kospi(request):
+    date = request.GET.get('date')
+    # print(type(date)) #str
+    cur_year = datetime.datetime.now().year
+    # print(type(cur_year)) #int
+    cur_month = datetime.datetime.now().month
+    cur_day = datetime.datetime.now().day
+
+    pd.set_option('precision', 4)
+
+    start = datetime.datetime(int(date[:4]), int(date[5:7]), int(date[8:]))
+    end = datetime.datetime(cur_year, cur_month, cur_day)
+    # start = datetime.datetime(2021, 5, 1)
+    # end = datetime.datetime(2021, 5, 14)
+    df_null = wb.DataReader("^KS11", "yahoo", start, end)
+    df = df_null.dropna()
+
+    kospi_chart = df.Close.plot(style='b')
+    kospi_chart.set_title("KOSPI")
+    kospi_chart.set_ylabel("코스피 지수")
+    kospi_chart.set_xlabel("일자")
+    kospi_chart.set_xlim(str(start), str(end))
+
+    # print(df)
+
+    # print("Close Median", df['Close'].median())
+    # print(df['Close'].describe())
+    # print(df.corr())
+
+    # plt.show()
+    plt.savefig("./stock/static/img/kospi.png")
+
+    return render(request, 'stock/test.html', {'date': date, })
