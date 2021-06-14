75ce1a073705ec98f48efa29eebd2e999caafebe
imjeongjun <jjlim1004@naver.com>
2021-06-14 18:16:34 +0900

    6.14
    Former-commit-id: e3c2e1f0476dae6d06c244e03eeb4ac74c1cf029

--- stock/urls.py
+++ stock/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('kospi/', views.stock_graph.as_view(), name='kospi'),
     path('stocklist/', views.stock_information.as_view(), name='stock_inform'),
     path('stock_detail/', views.stock_detail.as_view(), name='stock_detail'),
-    path('stock_predict/', views.stock_predict.as_view(), name='stock_predict'),
+    # path('stock_predict/', views.stock_predict.as_view(), name='stock_predict'),
 ]
